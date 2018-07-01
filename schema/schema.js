ffd4eb2434b8b4be138aba4d43d0946fc39b5b38
zitongliu <zitongliu7@gmail.com>
2018-07-01 18:31:09 +1000

    set up json server and call it via graphql

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 
 const {
   GraphQLObjectType,
@@ -8,10 +8,6 @@ const {
   GraphQLSchema
 } = graphql;
 
-const users = [
-    { id: '23', firstName: 'Bill', age: 20 },
-    { id: '47', firstName: 'Samantha', age: 21 }
-];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -29,7 +25,8 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString }},
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(response => response.data );
       }
     }
   }
